50ed6aafc212031dac27fce8fb06a8017ea66c9f
Alexander <juanperzezss@gmail.com>
2025-05-18 19:56:25 -0500

    Se desarrolla la coneccion con la api de Google cloud Text to speech y se obtiene el audio generado

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,24 +1,8 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-//import { onRequest } from "firebase-functions/v2/https"; descomentar
-//import * as logger from "firebase-functions/logger";  descomentar
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 import * as functions from 'firebase-functions';
 import { generatedContent } from './services/ia-service';
+import { TextToSpeechClient, protos } from '@google-cloud/text-to-speech';
+
+const ttsClient = new TextToSpeechClient();
 
 export const cursosDisponibles = functions.https.onRequest(async (request, response) => {
 
@@ -27,16 +11,28 @@ export const cursosDisponibles = functions.https.onRequest(async (request, respo
 
         if (!mensaje) {
             response.status(400).json({ error: 'Mensaje requerido' });
-            return ;
+            return;
         }
 
         const resultado = await generatedContent(mensaje);
         console.log(resultado);
 
-        response.status(200).json({ respuesta: resultado });
+        const ttsRequest = {
+            input: { text: resultado },
+            voice: { languageCode: 'es-ES', ssmlGender: protos.google.cloud.texttospeech.v1.SsmlVoiceGender.FEMALE },
+            audioConfig: { audioEncoding: protos.google.cloud.texttospeech.v1.AudioEncoding.MP3 },
+        };
+
+        const [ttsResponse] = await ttsClient.synthesizeSpeech(ttsRequest);
+        if (!ttsResponse.audioContent) {
+            throw new Error('No se pudo generar el audio');
+        }
+
+        response.setHeader('Content-Type', 'audio/mpeg');
+        response.setHeader('Content-Disposition', 'attachment; filename=respuesta.mp3');
+        response.send(ttsResponse.audioContent);
     } catch (error) {
         console.error(error);
         response.status(500).json({ error: 'Error interno del servidor' });
     }
-}
-);
+});
